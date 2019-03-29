ac02b19d7b4c1cb14dabf42b53dfec2596a1e989
Lukasz Danelczyk <lukasz.dan91@gmail.com>
2019-03-29 20:44:49 +0100

    Add new model class

--- src/main/java/pl/dels/model/Activity.java
+++ src/main/java/pl/dels/model/Activity.java
@@ -1,7 +1,145 @@
 package pl.dels.model;
 
-public class Activity {
-	
-	
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+import pl.dels.model.User;
+
+@Entity
+@Table(name = "activities")
+public class Activity implements Serializable {
+	private static final long serialVersionUID = -8046479600118986194L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(nullable = false, name = "machine_number")
+	private String machineNumber;
+	@Column(nullable = false)
+	private String side;
+	@Column(nullable = false, name = "activity_type")
+	private String activityType;
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	public Activity() {
+
+	}
+
+	public Activity(Long id, String machineNumber, String side, String activityType, User user) {
+		super();
+		this.id = id;
+		this.machineNumber = machineNumber;
+		this.side = side;
+		this.activityType = activityType;
+		this.user = user;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getMachineNumber() {
+		return machineNumber;
+	}
+
+	public void setMachineNumber(String machineNumber) {
+		this.machineNumber = machineNumber;
+	}
+
+	public String getSide() {
+		return side;
+	}
+
+	public void setSide(String side) {
+		this.side = side;
+	}
+
+	public String getActivityType() {
+		return activityType;
+	}
+
+	public void setActivityType(String activityType) {
+		this.activityType = activityType;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((activityType == null) ? 0 : activityType.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((machineNumber == null) ? 0 : machineNumber.hashCode());
+		result = prime * result + ((side == null) ? 0 : side.hashCode());
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Activity other = (Activity) obj;
+		if (activityType == null) {
+			if (other.activityType != null)
+				return false;
+		} else if (!activityType.equals(other.activityType))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (machineNumber == null) {
+			if (other.machineNumber != null)
+				return false;
+		} else if (!machineNumber.equals(other.machineNumber))
+			return false;
+		if (side == null) {
+			if (other.side != null)
+				return false;
+		} else if (!side.equals(other.side))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Activity [id=" + id + ", machineNumber=" + machineNumber + ", side=" + side + ", activityType="
+				+ activityType + ", user=" + user + "]";
+	}
 }
