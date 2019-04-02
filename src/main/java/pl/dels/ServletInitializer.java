8916fc3d1188ec9f12862fbf6e06b1dc3d04661c
Lukasz Danelczyk <lukasz.dan91@gmail.com>
2019-04-02 11:39:27 +0200

    Changed project name

--- src/main/java/pl/dels/ServletInitializer.java
+++ src/main/java/pl/dels/ServletInitializer.java
@@ -7,7 +7,6 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(WorkOptimizerApplication.class);
+		return application.sources(ActivityRecorderApplication.class);
 	}
-
 }
