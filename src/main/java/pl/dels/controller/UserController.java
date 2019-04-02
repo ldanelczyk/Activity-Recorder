8a28c0609b14ab8744032a10ff7cb4149d064bea
Lukasz Danelczyk <lukasz.dan91@gmail.com>
2019-04-02 11:39:41 +0200

    Changed method name

--- src/main/java/pl/dels/controller/UserController.java
+++ src/main/java/pl/dels/controller/UserController.java
@@ -3,20 +3,24 @@ package pl.dels.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import pl.dels.service.UserService;
+
 @Controller
 public class UserController {
 
 	@GetMapping("/login")
-	private String loginForm() {
+	private String login() {
+
 		return "login";
 	}
-	
+
 	@GetMapping("/logout")
 	private String logoutPage(HttpServletRequest request, HttpServletResponse response) {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
