300f78f7943034427f9f32067e815558e3f7cbd1
Lukasz Danelczyk <lukasz.dan91@gmail.com>
2019-04-02 11:37:39 +0200

    Changed annonation request type

--- src/main/java/pl/dels/controller/HomeController.java
+++ src/main/java/pl/dels/controller/HomeController.java
@@ -2,14 +2,13 @@ package pl.dels.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+	@RequestMapping("/")
     public String home(Model model) {
-        model.addAttribute("helloMessage", "Hello! :)");
         return "index";
     }
 }
