733d0a918c81557d3eb17bc1545c0f6b65d754a7
Lukasz Danelczyk <lukasz.dan91@gmail.com>
2019-04-03 21:53:06 +0200

    Test

--- src/main/java/pl/dels/controller/HomeController.java
+++ src/main/java/pl/dels/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
 	@RequestMapping("/")
-    public String home(Model model) {
-        return "index";
-    }
+	public String home(Model model) {
+		return "index";
+	}
 }
