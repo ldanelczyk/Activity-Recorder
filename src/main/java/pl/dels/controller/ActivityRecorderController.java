739018119d54d14643d8d273704168c997c022fa
Lukasz Danelczyk <lukasz.dan91@gmail.com>
2019-04-16 14:21:57 +0200

    Added provider for adding activities

---
+++ src/main/java/pl/dels/controller/ActivityRecorderController.java
@@ -0,0 +1,65 @@
+package pl.dels.controller;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import pl.dels.model.Activity;
+import pl.dels.service.ActivityRecorderService;
+
+@Controller
+public class ActivityRecorderController {
+
+	private ActivityRecorderService activityRecorderService;
+
+	@Autowired
+	private void setPostService(ActivityRecorderService activityRecorderService) {
+		this.activityRecorderService = activityRecorderService;
+	}
+
+	@PostMapping("/startRegistration")
+	private String startRegistration(@RequestParam String machineNumber, @RequestParam String workOrder,
+			@RequestParam String side, @RequestParam String activityType, Model model) {
+
+		activityRecorderService.setStartDateTime();
+
+		Activity activity = activityRecorderService.createTempActivity(machineNumber, workOrder, side, activityType);
+
+		System.out.println(activity);
+
+		model.addAttribute("activity", activity);
+
+		return "started";
+	}
+
+	@PostMapping("/stopRegistration")
+	private String stopRegistration(@RequestParam String machineNumber, @RequestParam String workOrder,
+			@RequestParam String side, @RequestParam String activityType, @RequestParam String comments) {
+
+		String nameOfLoggedUser = SecurityContextHolder.getContext().getAuthentication().getName();
+
+		Timestamp startDateTime = activityRecorderService.getStartDateTime();
+
+		Timestamp stopDateTime = new Timestamp(new Date().getTime());
+
+		double downtime = activityRecorderService.downtimeCounter(startDateTime, stopDateTime);
+
+		activityRecorderService.saveActivityInDatabase(machineNumber, workOrder, side, activityType, comments,
+				startDateTime, stopDateTime, downtime, nameOfLoggedUser);
+
+		return "redirect:addOk";
+	}
+
+	@GetMapping("/addOk")
+	private String addActivity() {
+
+		return "addOk";
+	}
+}
