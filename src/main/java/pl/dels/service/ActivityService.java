950ed93a579ce53f1acbf5924998aa01bedbc9f6
Lukasz Danelczyk <lukasz.dan91@gmail.com>
2019-05-24 13:09:20 +0200

    Added lombok annonations and removed setters

--- src/main/java/pl/dels/service/ActivityService.java
+++ src/main/java/pl/dels/service/ActivityService.java
@@ -6,41 +6,37 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
 import java.util.Comparator;
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import pl.dels.model.User;
 import pl.dels.model.Activity;
 import pl.dels.repository.ActivityRepository;
 import pl.dels.repository.UserRepository;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Service
 public class ActivityService {
 
+	@Autowired
 	private ActivityRepository activityRepository;
 
+	@Autowired
 	private UserRepository userRepository;
-
+	
 	private final double DIVIDER_OF_SECONDS = 1000000000;
 
 	private final double DIVIDER_OF_HOUR = 3600;
 
 	private final double ROUND_CONSTANCE = 1000;
 
-	@Autowired
-	public void setActivityRepository(ActivityRepository activityRepository) {
-		this.activityRepository = activityRepository;
-	}
-
-	@Autowired
-	public void setUserRepository(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
-
-	public void saveActivityInDatabase(String machineNumber, String workOrder, String side, String activityType,
+	public Activity saveActivityInDatabase(String machineNumber, String workOrder, String side, String activityType,
 			String comments, Timestamp startDateTime, Timestamp stopDateTime, double downtime,
 			String nameOfLoggedUser) {
 
@@ -57,7 +53,7 @@ public class ActivityService {
 				
 		User user = userRepository.findByUsername(nameOfLoggedUser);
 		activity.setUser(user);
-		activityRepository.save(activity);
+		return activityRepository.save(activity);
 	}
 
 	public List<Activity> getAllActivities(Comparator<Activity> comparator) {
@@ -76,7 +72,6 @@ public class ActivityService {
 							.side(side)
 							.activityType(activityType)
 							.build();
-
 		return activity;
 	}
 
--- src/main/java/pl/dels/service/UserService.java
+++ src/main/java/pl/dels/service/UserService.java
@@ -4,40 +4,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 import pl.dels.model.Role;
 import pl.dels.model.User;
 import pl.dels.repository.RoleRepository;
 import pl.dels.repository.UserRepository;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Service
 public class UserService {
 
 	private static final String DEFAULT_ROLE = "ROLE_USER";
+
+	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
 	private RoleRepository roleRepository;
 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 
-	@Autowired
-	public void setUserRepository(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
-
-	@Autowired
-	public void setRoleRepository(RoleRepository roleRepository) {
-		this.roleRepository = roleRepository;
-	}
-
-	public void saveUserInDatabase(String username, String password) {
+	public User saveUserInDatabase(String username, String password) {
 		String bcryptPass = passwordEncoder.encode(password);
 		User user = new User(username, bcryptPass);
 		addWithDefaultRole(user);
+		return user;
 	}
 
-	private void addWithDefaultRole(User user) {
+	public Role addWithDefaultRole(User user) {
 		Role defaultRole = roleRepository.findByRoleName(DEFAULT_ROLE);
 		user.getRoles().add(defaultRole);
 		userRepository.save(user);
+		return defaultRole;
 	}
 }
