8a86b00b71e35356809e7b6c1b6b79cb47c6fedd
Lukasz Danelczyk <lukasz.dan91@gmail.com>
2019-06-03 20:51:12 +0200

    Removed generate excel method

--- src/main/java/pl/dels/controller/UserController.java
+++ src/main/java/pl/dels/controller/UserController.java
@@ -5,25 +5,17 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import pl.dels.toolsprovider.XlsProvider;
-
 @Controller
 public class UserController {
 
-	@Autowired
-	private XlsProvider xlsProvider;
-
 	@GetMapping("/login")
 	private String login() throws IOException {
-
-		xlsProvider.generateExcelFile("C:\\Users\\danelczykl\\Desktop\\test.xlsx");
 
 		return "login";
 	}
