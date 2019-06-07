3cc31965e91ca76b034b7ce25216cb140b8f1e9b
Lukasz Danelczyk <lukasz.dan91@gmail.com>
2019-06-07 10:21:31 +0200

    Added model for chart

---
+++ src/main/java/pl/dels/model/ChartActivity.java
@@ -0,0 +1,17 @@
+package pl.dels.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ChartActivity {
+	
+	private String workOrder;
+	
+	private double downtime;
+}
