1fc282dfe5ee0090cd0e04a526a89cb4cbe2152d
Lukasz Danelczyk <lukasz.dan91@gmail.com>
2019-06-12 16:55:11 +0200

    Added class for static methods

---
+++ src/main/java/pl/dels/toolsprovider/MathToolProvider.java
@@ -0,0 +1,31 @@
+package pl.dels.toolsprovider;
+
+import java.time.LocalTime;
+
+public class MathToolProvider {
+	
+	public static double round(double value, int places) {
+
+		if (places < 0)
+
+			throw new IllegalArgumentException();
+
+		long factor = (long) Math.pow(10, places);
+		
+		value = value * factor;
+		
+		long tmp = Math.round(value);
+
+		return (double) tmp / factor;
+	}
+	
+	public static String convertTimeToFileName(LocalTime localTime) {
+		
+		String localTimeToConvert = String.valueOf(localTime);
+		
+		String localTimeConverted = localTimeToConvert.replace(':', '_');
+		
+		return localTimeConverted;
+		
+	}
+}
