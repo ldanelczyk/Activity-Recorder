301f96bdec52bdac40b7e55809fb023bc1083af1
Lukasz Danelczyk <lukasz.dan91@gmail.com>
2019-06-17 23:59:22 +0200

    Added empty line

--- src/test/java/pl/dels/controller/ActivityControllerTest.java
+++ src/test/java/pl/dels/controller/ActivityControllerTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
