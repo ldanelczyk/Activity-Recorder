b1753ed6da1b3a43b4502104938ea4a7dc372750
Lukasz Danelczyk <lukasz.dan91@gmail.com>
2019-06-17 23:57:51 +0200

    Changed class name

--- src/main/java/pl/dels/toolsprovider/ToolProvider.java
+++ src/main/java/pl/dels/toolsprovider/ToolProvider.java
@@ -2,8 +2,8 @@ package pl.dels.toolsprovider;
 
 import java.time.LocalTime;
 
-public class MathToolProvider {
-	
+public class ToolProvider {
+
 	public static double round(double value, int places) {
 
 		if (places < 0)
@@ -11,21 +11,20 @@ public class MathToolProvider {
 			throw new IllegalArgumentException();
 
 		long factor = (long) Math.pow(10, places);
-		
+
 		value = value * factor;
-		
+
 		long tmp = Math.round(value);
 
 		return (double) tmp / factor;
 	}
-	
+
 	public static String convertTimeToFileName(LocalTime localTime) {
-		
+
 		String localTimeToConvert = String.valueOf(localTime);
-		
+
 		String localTimeConverted = localTimeToConvert.replace(':', '_');
-		
+
 		return localTimeConverted;
-		
 	}
 }
