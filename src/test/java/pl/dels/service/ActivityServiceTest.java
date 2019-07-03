a91a2e552558b841f853742aafa719eb66b717dc
Lukasz Danelczyk <lukasz.dan91@gmail.com>
2019-07-03 14:36:35 +0200

    Added tests for new service method

--- src/test/java/pl/dels/service/ActivityServiceTest.java
+++ src/test/java/pl/dels/service/ActivityServiceTest.java
@@ -12,8 +12,10 @@ import org.junit.jupiter.api.Test;
 
 import org.mockito.ArgumentCaptor;
 
+import java.io.IOException;
 import java.sql.Timestamp;
-
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -23,13 +25,15 @@ import pl.dels.database.dao.ActivityDaoImpl;
 import pl.dels.database.repository.ActivityRepository;
 import pl.dels.database.repository.UserRepository;
 import pl.dels.model.Activity;
+import pl.dels.model.ChartActivity;
 import pl.dels.model.enums.MachineNumber;
 import pl.dels.model.enums.Side;
 import pl.dels.toolsprovider.TimeToolsProvider;
 
 class ActivityServiceTest {
+	
 
-	private Timestamp startDateTime = new Timestamp(new Date().getTime());
+	private Timestamp startDateTime = Timestamp.valueOf("2019-06-20 10:02:00");
 
 	private Timestamp stopDateTime = new Timestamp(new Date().getTime());
 	
@@ -59,7 +63,7 @@ class ActivityServiceTest {
 	}
 	
 	@Test
-	void getAllActivitiesSortedWithComparator() {
+	void getAllActivitiesFromMySql() {
 
 		//given
 		List<Activity> createdActivityList = createActivities();
@@ -75,9 +79,56 @@ class ActivityServiceTest {
 		
 		//then
 		verify(activityRepository, times(1)).findAll();
-		assertThat(returnedActivityList, hasSize(3));
+		assertThat(returnedActivityList, hasSize(4));
 		assertThat(returnedActivityList.get(0).getWorkOrder(), equalTo("ZRXXXX"));
 		assertEquals(createdActivityList, returnedActivityList);
+	}
+	
+	@Test
+	void getProcessedChartActivitiesFromMySql() {
+		
+		//given
+		List<Activity> createdActivityList = createActivities();
+				
+		ActivityRepository activityRepository = mock(ActivityRepository.class);
+				
+		ActivityService activityService = new ActivityService(activityRepository);
+				
+		given(activityRepository.findAll()).willReturn(createdActivityList);
+		
+		//when
+		List<ChartActivity> returnedChartActivityList = activityService.getProcessedChartActivitiesFromMySql();
+		
+		//then
+		verify(activityRepository, times(1)).findAll();
+		assertThat(returnedChartActivityList, hasSize(3));
+		assertThat(returnedChartActivityList.get(0).getWorkOrder(), equalTo("ZRXXXX"));
+		assertNotEquals(createdActivityList, returnedChartActivityList);	
+	}
+	
+	@Test
+	void getProcessedChartActivitiesFromFirebird() throws ClassNotFoundException, IOException {
+		
+		//given
+		List<ChartActivity> createdChartActivityList = createChartActivities();
+		
+		ActivityDao activityDao = mock(ActivityDao.class);
+		
+		ActivityService activityService = new ActivityService(activityDao);
+		
+		String mondayOfTheWeek = "2019-06-18";
+		String sundayOfTheWeek = "2019-06-26";
+				
+		given(activityDao.getAllActivities(mondayOfTheWeek, sundayOfTheWeek)).willReturn(createdChartActivityList);
+		
+		//when
+		List<ChartActivity> returnedChartActivityList = activityService.getProcessedChartActivitiesFromFirebird();
+		
+		//then
+		verify(activityDao, times(1)).getAllActivities(mondayOfTheWeek, sundayOfTheWeek);
+		assertThat(returnedChartActivityList, hasSize(6));
+		assertThat(returnedChartActivityList.get(0).getWorkOrder(), equalTo("ZRXXXX"));
+		assertNotEquals(createdChartActivityList, returnedChartActivityList);		
 	}
 	
 	@Test
@@ -96,9 +147,7 @@ class ActivityServiceTest {
 	@Test
 	void downtimeCounterShouldReturnDowntimeBetweenTwoDates() throws InterruptedException {
 		
-		//given
-		ActivityService activityService = new ActivityService();
-		
+		//given		
 		Timestamp startDateTime = new Timestamp(2019, 5, 23, 21, 00, 10, 0);
 		
 		Timestamp stopDateTime = new Timestamp(2019, 5, 23, 21, 30, 10, 0);
@@ -161,6 +210,33 @@ class ActivityServiceTest {
 				.downtime(10.008)
 				.build();
 		
-		return Arrays.asList(activity1, activity2, activity3);
+		Activity activity4 = Activity.builder()
+				.machineNumber(MachineNumber.AOI2)
+				.workOrder("ZRYYYX")
+				.side(Side.BOT)
+				.activityType("pisanie programu AOI")
+				.comments("Example Comment1")
+				.startDateTime(Timestamp.valueOf("2019-06-15 10:02:00"))
+				.stopDateTime(stopDateTime)
+				.downtime(10.008)
+				.build();
+		
+		return Arrays.asList(activity1, activity2, activity3, activity4);
+	}
+	
+	private List<ChartActivity> createChartActivities() {
+		
+		ChartActivity activity1 = new ChartActivity("ZRXXXX", 1);
+		ChartActivity activity2 = new ChartActivity("ZRXXXX", 1);
+		ChartActivity activity3 = new ChartActivity("ZRXXYY", 2);
+		ChartActivity activity4 = new ChartActivity("ZRXXYY", 2);
+		ChartActivity activity5 = new ChartActivity("ZRYYYY", 3);
+		ChartActivity activity6 = new ChartActivity("ZRYYYY", 3);
+		ChartActivity activity7 = new ChartActivity("ZRYYYX", 4);
+		ChartActivity activity8 = new ChartActivity("ZRYYYX", 4);
+		ChartActivity activity9 = new ChartActivity("ZRZZZZ", 1);
+		ChartActivity activity10 = new ChartActivity("ZRZZYX", 1);
+		
+		return Arrays.asList(activity1, activity2, activity3, activity4, activity5, activity6, activity7, activity8, activity9, activity10);
 	}
 }
