ab99baf08d6807512d2a809c7e43ccd171cff929
Lukasz Danelczyk <lukasz.dan91@gmail.com>
2019-07-22 19:57:58 +0200

    Refactored test method, add new test class

--- src/test/java/pl/dels/service/ActivityServiceTest.java
+++ src/test/java/pl/dels/service/ActivityServiceTest.java
@@ -144,21 +144,6 @@ class ActivityServiceTest {
 		assertNotNull(tempActivity);
 	}
 	
-	@Test
-	void downtimeCounterShouldReturnDowntimeBetweenTwoDates() throws InterruptedException {
-		
-		//given		
-		Timestamp startDateTime = new Timestamp(2019, 5, 23, 21, 00, 10, 0);
-		
-		Timestamp stopDateTime = new Timestamp(2019, 5, 23, 21, 30, 10, 0);
-		
-		//when
-		double downtime = TimeToolsProvider.downtimeCounter(startDateTime, stopDateTime);
-		
-		//then
-		assertThat(downtime, equalTo(0.5));
-	}
-	
 	private Activity createActivity() {
 		
 		Activity activity = Activity.builder()
