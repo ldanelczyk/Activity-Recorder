e31b5301f9b55ad631cd2ca017da22b788298f0f
Lukasz Danelczyk <lukasz.dan91@gmail.com>
2019-07-23 14:52:52 +0200

    Added empty line

--- src/test/java/pl/dels/controller/ActivityControllerTest.java
+++ src/test/java/pl/dels/controller/ActivityControllerTest.java
@@ -17,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 import org.mockito.InjectMocks;
